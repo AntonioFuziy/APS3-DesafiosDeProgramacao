b14d43c983783b302c1c8675345806c8477ce97a
AntonioFuziy <antoniovf@al.insper.edu.br>
2021-04-02 19:30:37 -0300

    tentativa integration

--- dna.c
+++ dna.c
@@ -27,7 +27,7 @@ int mlcs_w(char a[], int n, char b[], int m, int length[MAX_SIZE + 1][MAX_SIZE +
     if(m == 0 || n == 0){
         return 0;
     } else if(a[n-1] == b[m-1]){
-        return 1 + mlcs_w(a, n-1, b, m-1, length);
+        return mlcs_w(a, n-1, b, m-1, length) + 1;
     }
 
     return max(mlcs_w(a, n-1, b, m, length), mlcs_w(a, n, b, m-1, length));
@@ -54,5 +54,24 @@ int mlcs(char a[], int n, char b[], int m) {
 
 
 int dlcs(char a[], int n, char b[], int m) {
-    return 2;
+    int length[n][m];
+
+    for(int l = 0; l < m; l++){
+        for(int i = 0; i < n; i++){
+            length[l][i] = a[i];
+        }   
+    }
+
+    for(int c = 0; c < n; c++){
+        for(int j = 0; j < m; j++){
+            length[j][c] = b[j];
+        }
+    }
+
+    if(n == 0 || m == 0){
+        return 0;
+    } else if(a[n-1] == b[m-1]){
+        return length[n-1][m-1] + 1;
+    }
+    return max(length[n-1][m], length[n][m-1]);
 }
